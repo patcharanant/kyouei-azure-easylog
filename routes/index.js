3a1cc040362b21b6592c4eb955182844149ef9a7
patcharanant <t.tann1234@gmail.com>
2022-07-13 17:21:37 +0700

    - add file system module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var moment = require('moment-timezone');
 
+moment().tz("Asia/Bangkok").format();
 console.log('init index router')
 router.post('/easylog_1', function(req, res, next) {
-  console.log('in')
   try {
     console.log('request body is ===============================')
-    console.log(req.body);
-    res.status(200).send('wut');
+    console.log(req.body)
+    let jsonData = JSON.stringify(req.body)
+    let dateNow = moment().format('MM-DD-YYYY(HH:mm:ss)')
+    console.log(dateNow)
+    fs.writeFile(`temp/${dateNow}.json`, jsonData, function(error) {
+      if (error) {
+          console.log('write file error')
+          console.log(error);
+      } 
+    });
+    console.log('waiting to delete')
+    // setTimeout( function(){
+    //     fs.unlink(`temp/${dateNow}.json`, (err) => {
+    //       if (err) {
+    //         console.error(err)
+    //         return
+    //       }
+    //     })
+    //   console.log('deleted')
+    // }, 5000)
+    
+    res.status(200).send('OK');
   } catch (err) {
     console.log(err)
     res.status(500).send('error');
